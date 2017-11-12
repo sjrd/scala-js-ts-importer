5de85046a48cdc61c53ce6ca2bb8bcdb8e8d3484
exoego <ytatsunojp@gmail.com>
2017-11-12 19:11:12 +0900

    Fetch sample type definition so that users can give it a try.

--- src/main/scala/org/scalajs/tools/tsimporter/Main.scala
+++ src/main/scala/org/scalajs/tools/tsimporter/Main.scala
@@ -6,22 +6,25 @@
 package org.scalajs.tools.tsimporter
 
 import blog.codeninja.scalajs.vue._
-import org.scalajs.dom.console
-import org.scalajs.dom.MessageEvent
 import org.scalajs.dom.webworkers.Worker
+import org.scalajs.dom.{MessageEvent, console}
 
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
-
 class Data(var input: Input,
            var isLoading: Boolean,
-           var output: ScalaOutput) extends js.Object {
+           var output: ScalaOutput,
+           var samples: js.Array[Sample]
+          ) extends js.Object {
 }
 
 class Input(var source: String, var outputPackage: js.UndefOr[String]) extends js.Object
+
 class ScalaOutput(var text: String, var hasError: Boolean) extends js.Object
+
+class Sample(var filename: String, var label: String) extends js.Object
 
 @JSExportTopLevel("Main")
 object Main {
@@ -49,6 +52,9 @@ object Main {
       output = new ScalaOutput(
         text = "",
         hasError = false
+      ),
+      samples = js.Array(
+        new Sample("elasticsearch", "elasticsearch")
       )
     )
 
@@ -64,14 +70,37 @@ object Main {
       }
     }
 
+    def translate(data: Data): Unit = {
+      console.info("[Main] send to worker")
+      data.isLoading = true
+      worker.postMessage(data.input)
+    }
+
     vue = new Vue(js.Dynamic.literal(
       el = "#app",
       data = data,
       methods = js.Dynamic.literal(
-        translate = js.ThisFunction.fromFunction1 { data: Data =>
-          console.info("[Main] send to worker")
+        translate = translate _: js.ThisFunction0[Data, _],
+        loadSample = js.ThisFunction.fromFunction2 { (data: Data, sample: Sample) =>
+          import org.scalajs.dom.experimental._
           data.isLoading = true
-          worker.postMessage(data.input)
+
+          val fetchSampleTypeScript = Fetch.fetch(s"samples-webapp/${sample.filename}.d.ts").toFuture
+          val futureSourceText = fetchSampleTypeScript.flatMap { response =>
+            response.text().toFuture.map { text =>
+              response.status match {
+                case 200 => text
+                case _ => s"""Failed to fetch sample.
+                             |
+                             |Error response:
+                             |${text}""".stripMargin
+              }
+            }
+          }
+          futureSourceText.foreach { text =>
+            data.input.source = text
+            translate(data)
+          }
         }
       )
     ))
