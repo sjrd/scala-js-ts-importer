8c7605633d08aa2327801ac9f1a3240cb28fbfc7
Brian P. Holt <bholt@dwolla.com>
2018-02-14 17:58:47 -0600

    Fix #70: allow leading | in union type definitions

---
+++ samples/uniontype.d.ts.scala
@@ -0,0 +1,20 @@
+
+import scala.scalajs.js
+import js.annotation._
+import js.|
+
+package uniontype {
+
+package uniontype {
+
+@js.native
+@JSGlobal("uniontype")
+object Uniontype extends js.Object {
+  type NumberOrString = Double | String
+  type LeadingPipe = Double | String
+  type MultilineLeadingPipe = Double | String
+}
+
+}
+
+}
