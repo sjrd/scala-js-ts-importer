d63482b6851169456e6701932a8794bdaf2b082d
exoego <mogami@exoego.net>
2018-03-23 21:41:29 +0900

    Clean up test

--- samples/indexabletypes.d.ts
+++ samples/indexabletypes.d.ts
@@ -3,10 +3,5 @@ interface NumberDictionary {
     [index: string]: number;
 }
 interface ReadonlyStringArray {
-    [index: number]: string;
-}
-
-// extract example from https://github.com/Microsoft/vscode/blob/bc7b804fdec62173b5437d188e6aa64c036d24f0/src/vs/vscode.d.ts#L3601
-export interface WorkspaceConfiguration {
-    readonly [key: string]: any;
+    readonly [index: number]: string;
 }
--- samples/indexabletypes.d.ts.scala
+++ samples/indexabletypes.d.ts.scala
@@ -17,14 +17,6 @@ trait NumberDictionary extends js.Object {
 trait ReadonlyStringArray extends js.Object {
   @JSBracketAccess
   def apply(index: Double): String = js.native
-  @JSBracketAccess
-  def update(index: Double, v: String): Unit = js.native
-}
-
-@js.native
-trait WorkspaceConfiguration extends js.Object {
-  @JSBracketAccess
-  def apply(key: String): js.Any = js.native
 }
 
 }
