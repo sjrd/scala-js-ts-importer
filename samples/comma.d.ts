1fbed7a993cfeac19d11d791eb3b9e8c1c396637
TATSUNO Yasuhiro <mogami@exoego.net>
2019-05-07 19:59:25 +0900

    Allow trailing comma in type in function signature (#96)

---
+++ samples/comma.d.ts
@@ -0,0 +1,14 @@
+export class Foo {
+    foo(
+        options: {
+            key1: string,
+            key2: string,
+        },
+        key3: string,
+    ): void;
+}
+
+export interface Bar {
+    key1: string,
+    key2: string,
+}
