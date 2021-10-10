8597524af08788f97af2540fa56452ab07c6b0ae
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-11 06:49:05 +0800

    Delete samples directory

--- samples/duplicateliteraltypes.d.ts
+++
@@ -1,9 +0,0 @@
-declare module duplicateliteraltypes {
-    export interface duplicateliteraltypes {
-        duplicate(input: true): void;
-        duplicate(input: false): void;
-
-        duplicate(input: "hello"): void;
-        duplicate(input: string): void;
-    }
-}
